61d059dc71c077639744d38e01aafd95313f5982
Larissa Simões <larissacarneiro2@hotmail.com>
2023-10-09 12:38:49 -0300

    feat: Adiciona JavaDoc no MuseumNotFoundException

--- src/main/java/com/betrybe/museumfinder/exception/MuseumNotFoundException.java
+++ src/main/java/com/betrybe/museumfinder/exception/MuseumNotFoundException.java
@@ -1,5 +1,8 @@
 package com.betrybe.museumfinder.exception;
 
+/**
+ * Exception to be thrown when a museum is not found.
+ */
 public class MuseumNotFoundException extends RuntimeException {
   public MuseumNotFoundException(String message) {
     super(message);
