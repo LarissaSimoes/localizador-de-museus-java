e014ae15e8351fc61ae75340fc19b8713597902a
Larissa Simões <larissacarneiro2@hotmail.com>
2023-10-09 16:27:00 -0300

    feat: Implementa método getClosestMuseum na camada Controller

--- src/main/java/com/betrybe/museumfinder/controller/MuseumController.java
+++ src/main/java/com/betrybe/museumfinder/controller/MuseumController.java
@@ -1,15 +1,18 @@
 package com.betrybe.museumfinder.controller;
 
 import com.betrybe.museumfinder.dto.MuseumDto;
+import com.betrybe.museumfinder.model.Coordinate;
 import com.betrybe.museumfinder.model.Museum;
 import com.betrybe.museumfinder.service.MuseumServiceInterface;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 /**
  * Museum controller.
@@ -35,6 +38,22 @@ public class MuseumController {
     }
     return new ResponseEntity<>(museum, HttpStatus.CREATED);
   }
+
+  /**
+   * Get the closest museum.
+   */
+  @GetMapping("/closest")
+  public ResponseEntity<Museum> getClosestMuseum(@RequestParam Double lat,
+                                                 @RequestParam Double lng,
+                                                 @RequestParam(name = "max_dist_km") Double maxDistance) {
+    Coordinate coordinate = new Coordinate(lat, lng);
+    Museum museum = this.museumService.getClosestMuseum(coordinate, maxDistance);
+    if (museum == null) {
+      return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+    return new ResponseEntity<>(museum, HttpStatus.OK);
+  }
+
 
 
 }
